799cb8212729ef6697cfab3e795c44b5683876c1
QOHARY <114688163+QOHARY@users.noreply.github.com>
2023-10-02 23:10:27 +0800

    Delete MODUL 0 directory

--- MODUL 0/PRAK001_2210817210014_MuhammadQoharyRawidafany.java
+++
@@ -1,7 +0,0 @@
-public class PRAK001_2210817210014_MuhammadQoharyRawidafany{
-    public static void main(String[] args) {
-     System.out.println("Selamat Pagi, Muhammad Qohary Rawidafany");
-     System.out.println("Selamat Siang, Muhammad Qohary Rawidafany");
-     System.out.println("Selamat Malam, Muhammad Qohary Rawidafany");
-      }
-}
--- MODUL 0/PRAK004_2210817210014_MuhammadQoharyRawidafany.java
+++
@@ -1,9 +0,0 @@
-public class PRAK004_2210817210014_MuhammadQoharyRawidafany{
-    public static void main(String[] args) {
-    String nama = "Muhammad Qohary Rawidafany";
-    int umur = 12 ;
-     System.out.println("Perkenalkan, Nama Saya " + nama + ", Umur Saya " + umur);
-     System.out.println("Perkenalkan, Nama Saya "+ nama + ", Umur Saya " + umur);
-     System.out.println("Perkenalkan, Nama Saya "+ nama + ", Umur Saya " + umur);
-      }
-}
