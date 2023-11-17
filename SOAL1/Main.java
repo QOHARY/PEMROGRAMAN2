49daa26aa3d87f2691bd866a821eb7c28cc6f0a4
QOHARY <114688163+QOHARY@users.noreply.github.com>
2023-11-18 01:00:40 +0800

    Add files via upload

---
+++ SOAL1/Main.java
@@ -0,0 +1,8 @@
+package SOAL1;
+
+public class Main {
+    public static void main(String[] args) {
+        HewanPeliharaan hewanPeliharaan = new HewanPeliharaan();
+        hewanPeliharaan.display();
+    }
+}
