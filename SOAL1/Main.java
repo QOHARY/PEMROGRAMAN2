06d5a793aa23266208478a53bdc3e1b14f3fa412
QOHARY <114688163+QOHARY@users.noreply.github.com>
2023-11-21 21:27:00 +0800

    Delete SOAL1 directory

--- SOAL1/Main.java
+++
@@ -1,8 +0,0 @@
-package SOAL1;
-
-public class Main {
-    public static void main(String[] args) {
-        HewanPeliharaan hewanPeliharaan = new HewanPeliharaan();
-        hewanPeliharaan.display();
-    }
-}
